7e321668b7867c9ed6c752a7804ecebfab73c8a9
Dave <DavidRockin@users.noreply.github.com>
2019-08-18 17:18:02 -0400

    Update GlossChatBubbles.java

--- src/main/java/com/volmit/gloss/chatbubbles/GlossChatBubbles.java
+++ src/main/java/com/volmit/gloss/chatbubbles/GlossChatBubbles.java
@@ -57,7 +57,7 @@ public class GlossChatBubbles extends MortarPlugin implements Listener
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void on(AsyncPlayerChatEvent e)
 	{
-		if(e.isCancelled() || Config.blacklist.contains(e.getPlayer().getLocation().getWorld().getName()) ||
+		if(e.isCancelled() || (Config.blacklist != null && Config.blacklist.contains(e.getPlayer().getLocation().getWorld().getName())) ||
 					!e.getPlayer().hasPermission("gloss.chatbubbles.allow"))
 		{
 			return;
